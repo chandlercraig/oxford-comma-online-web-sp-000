203e6478e79d8d3cd11a2170de68b7853f65a8f6
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-24 17:15:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     return array.join
   elsif array.length == 2
-    return array.join(" and ")
+    return array.join("and ")
   elsif array.length > 2
     new_arr = []
     ox_com = " and " + array.pop
